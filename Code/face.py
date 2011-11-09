b938aab572715c83f189f2d81f0c9e29a4a39017
Yasser Souri <yassersouri@gmail.com>
2011-11-09 23:18:37 +0330

    added capturing capability to face.py

--- Code/face.py
+++ Code/face.py
@@ -1,11 +1,23 @@
 import cv
 
 face_cascade = "haarcascades/haarcascade_frontalface_alt.xml"
+debug = False
+count = 1
 
 def main():
 	hc = cv.Load(face_cascade)
 	cv.NamedWindow("camera", 1)
 	capture = cv.CaptureFromCAM(0)
+
+	print "Press ESC to exit"
+	print "capture image with keys 1 to 7"
+	print "1 : Neutral"
+	print "2: anger"
+	print "3: disgust"
+	print "4: fear"
+	print "5: happy"
+	print "6: sadness"
+	print "7: surprise"
 
 	while True:
 		img = cv.QueryFrame(capture)
@@ -25,9 +37,33 @@ def main():
 
 		if (key_pressed == 27):
 			break
+		elif (key_pressed == 49):
+			save_img("neutral", img, img_r)
+		elif (key_pressed == 50):
+			save_img("anger", img, img_r)
+		elif (key_pressed == 51):
+			save_img("disgust", img, img_r)
+		elif (key_pressed == 52):
+			save_img("fear", img, img_r)
+		elif (key_pressed == 53):
+			save_img("happy", img, img_r)
+		elif (key_pressed == 54):
+			save_img("sadness", img, img_r)
+		elif (key_pressed == 55):
+			save_img("surprise", img, img_r)
 
 	cv.DestroyWindow("camera")
 	cv.DestroyWindow("normalized")
+
+
+def save_img(img_type, img_o, img_f):
+	subdir = "data/"
+	global count
+	cv.SaveImage(subdir + "o_" + img_type + "_" + str(count) + ".jpg", img_o)
+	cv.SaveImage(subdir + "f_" + img_type + "_" + str(count) + ".jpg", img_f)
+	print "saved " + img_type + ": " + str(count)
+
+	count = count + 1
 	
 
 #return the 64 to 64 image
@@ -62,13 +98,15 @@ def handel_camera_image(img, hc):
 	number_of_faces = len(objects)
 
 	if number_of_faces != 1:
-		print "Error! Number of detected faces: " + str(number_of_faces)
+		if debug:
+			print "Error! Number of detected faces: " + str(number_of_faces)
 		return None
 	else:
 		for (x, y, w, h), n in objects:
 			#annotate the image
 			cv.Rectangle(img_f, (x,y), (x+w, y+h), 255)
-			print "FACE -> h: " + str(h) + ", w: " + str(w) + ", r(w/h): " + str(float(w)/float(h))
+			if debug:
+				print "FACE -> h: " + str(h) + ", w: " + str(w) + ", r(w/h): " + str(float(w)/float(h))
 			
 			#resize to 64 to 64
 			img_r = resize_crop_img(img_f, x,y,w,h)
