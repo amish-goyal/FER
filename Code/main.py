70942887b7062982dc929f97fe26456513b3c29a
Yasser Souri <yassersouri@gmail.com>
2011-11-17 19:57:44 +0330

    add a new testing way that works with pressing space

--- Code/main.py
+++ Code/main.py
@@ -11,8 +11,67 @@ def main():
 	# svm_params = "-t 0 -c 10"
 	# expample_make_model(img_kind, svm_params)
 
-	test_model(img_kind)
-	
+	# test_model(img_kind)
+
+	new_test()
+
+def new_test():
+	# load all the models
+	print "Loading Models"
+	happy_model = svmutil.svm_load_model('happy.model')
+	print 1
+	surprise_model = svmutil.svm_load_model('surprise.model')
+	print 2
+	anger_model = svmutil.svm_load_model('anger.model')
+	print 3
+	print "---------------------"
+
+	print "Loading cascade"
+	face_cascade = "haarcascades/haarcascade_frontalface_alt.xml"
+	hc = cv.Load(face_cascade)
+	print "---------------------"
+
+	capture = cv.CaptureFromCAM(0)
+	while True:
+		img = cv.QueryFrame(capture)
+		cv.ShowImage("camera",img)
+		key_pressed = cv.WaitKey(50)
+		if key_pressed == 27:
+			break
+		elif key_pressed == 32:
+			print '~> KEY PRESSED <~'
+			# do face detection
+			print 'detecting face'
+			returned = face.handel_camera_image(img, hc)
+			if returned == None:
+				print "No face || more than one face"
+				pass
+			else:
+				(img_o, img_face) = returned
+				cv.ShowImage("face",img_face)
+				# get features from the face
+				test_data = get_image_features(img_face)
+				predict_input_data = []
+				predict_input_data.append(test_data)
+
+				# do svm query for all models
+				(val, val_2, happy_label) = svmutil.svm_predict([1] ,predict_input_data, happy_model)
+				(val, val_2, surprise_label) = svmutil.svm_predict([1] ,predict_input_data, surprise_model)
+				(val, val_2, anger_label) = svmutil.svm_predict([1] ,predict_input_data, anger_model)
+
+				happy_val = happy_label[0][0]
+				surprise_val = surprise_label[0][0]
+				anger_val = anger_label[0][0]
+
+				if happy_val >= surprise_val and happy_val >= anger_val:
+					print "happy: "
+				if surprise_val >= happy_val and surprise_val >= anger_val:
+					print "surprise: "
+				if anger_val >= surprise_val and anger_val >= happy_val:
+					print "anger: "
+
+			print "---------------------"
+
 def test_model(img_kind):
 	model = svmutil.svm_load_model(img_kind + '.model')
 	print "Finished Loading Model"
